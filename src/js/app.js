c03643ea6e68f4c33e777787b5adee14d684c1d2
UlaNawrot <ula.nawrot@gmail.com>
2021-04-16 12:53:45 +0200

    adding photos to the website

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,14 @@ import "../scss/main.scss";
 import "../images/search_icon.png";
 import "../images/icon_search.svg";
 import "../images/white_search_icon.svg";
+import "../images/cochem_germany.jpg";
+import "../images/dinant_belgium.jpg"
+import "../images/hallstatt_austria.jpg"
+import "../images/venice_italy.jpg"
+import "../images/netherland.jpg"
+import "../images/rome_italy.jpg"
+import "../images/venice_italy.jpg"
+import "../images/photo_nav@2x.png"
 
 
 //test();
