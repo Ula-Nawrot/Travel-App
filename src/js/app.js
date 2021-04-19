a7bdc92bc3cd6dab669073ac6e8dbfbbb4fababa
UlaNawrot <ula.nawrot@gmail.com>
2021-04-19 14:59:41 +0200

    adding HTML and SCSS to header and cities to page 2

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,7 @@ import "../images/netherland.jpg"
 import "../images/rome_italy.jpg"
 import "../images/venice_italy.jpg"
 import "../images/photo_nav@2x.png"
+import "../images/arrow_back.svg"
 
 
 //test();
