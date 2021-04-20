83f6f95dda526a2e91c86a9994696727d84deed1
UlaNawrot <ula.nawrot@gmail.com>
2021-04-20 14:22:19 +0200

    Adding RWD to page 2

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,7 @@ import "../images/rome_italy.jpg"
 import "../images/venice_italy.jpg"
 import "../images/photo_nav@2x.png"
 import "../images/arrow_back.svg"
+import "../images/stars_1.svg"
 
 
 //test();
